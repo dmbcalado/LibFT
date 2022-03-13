f427459b67aa693058a1e659950396a53d8fa4c2
Diogo Calado <diogombcalado@gmail.com>
2022-03-13 01:00:52 +0000

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -19,18 +19,14 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	i = 0;
 	j = 0;
 	while (dest[i] && i < size)
-	{
 		i++;
-	}
 	while (src[j] && (j + i + 1) < size)
 	{
 		dest[i + j] = src[j];
 		j++;
 	}
 	if (i < size)
-	{
 		dest[i + j] = '\0';
-	}
 	return (i + ft_strlen((char *)src));
 }
 
